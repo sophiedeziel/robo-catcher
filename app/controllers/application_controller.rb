729c48e57d8492c22421e450fd2b1bda01bbf996
Sophie Déziel <courrier@sophiedeziel.com>
2019-02-10 19:48:57 -0500

    begin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   def start_process(name)
     if $robot_pid
-      Process.kill("HUP", $robot_pid)
-      Process.wait
-      $robot_pid = nil
+      begin
+        Process.kill("HUP", $robot_pid)
+        Process.wait
+      rescue
+      ensure
+        $robot_pid = nil
+      end
     end
     $robot_pid = Process.fork do
       exec(name)

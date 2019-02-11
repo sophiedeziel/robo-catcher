504dfcb107a6a6a71bb00343bb97f612af57b87f
Sophie Déziel <courrier@sophiedeziel.com>
2019-02-10 20:12:00 -0500

    le safety marche pas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
   def start_process(name)
-    if $robot_pid
-      begin
-        Process.kill("HUP", $robot_pid)
-        Process.wait
-      rescue
-      ensure
-        $robot_pid = nil
-      end
-    end
     $robot_pid = Process.fork do
       exec(name)
     end

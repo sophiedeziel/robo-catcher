6dbfc23ab39973290ea4135c4169a93a4a2e06a4
Sophie Déziel <courrier@sophiedeziel.com>
2022-01-13 00:13:39 -0500

    Assigner une valeur à un registre

---
+++ app/models/instruction/set_register.rb
@@ -0,0 +1,26 @@
+class Instruction
+  class SetRegister < Instruction
+    def self.human_name
+      "Assigner"
+    end
+    
+    def register_id
+      params["register_id"].to_i
+    end
+
+    def value
+      params["value"].to_i
+    end
+
+    def register
+      Register.find(register_id)
+    end
+
+    def execute(robot)
+      super do
+        register.update(value: value)
+        sleep 0.5
+      end
+    end
+  end
+end
